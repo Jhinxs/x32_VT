c4f7fb68d4a2443c4a06958b83172f333f50336b
陈枭 <79248856+axstorm@users.noreply.github.com>
2021-09-14 18:15:06 +0800

    new

--- x32_VT.c
+++ x32_VT.c
@@ -8,7 +8,6 @@ NTSTATUS DrvUnLoad(PDRIVER_OBJECT pDriver)
 {
 	
 	StopVT();
-
 	RecoverGDT();
 	DbgPrint("[+] Driver Unload Success!\n");
 	return STATUS_SUCCESS;
@@ -19,30 +18,28 @@ NTSTATUS DriverEntry(PDRIVER_OBJECT pDriver, PUNICODE_STRING pPath)
 {
 	DbgPrint("[+] Driver load Success!\n");
 	pDriver->DriverUnload = DrvUnLoad;
-	//*(ULONG*)(buffer + 2) = Asm_GetGdtBase();
-	//*(USHORT*)buffer = Asm_GetGdtLimit();
 	SaveGDT();
 	StartVT();
 	DbgPrint("vm-over\n");
 	return STATUS_SUCCESS;
 }
-VOID RecoverGDT() 
+VOID RecoverGDT()
 {
 	CHAR buffer[6];
-	
+
 	for (int i = 0; i < KeNumberProcessors; i++)
 	{
 		KeSetSystemAffinityThread((KAFFINITY)(1 << i));
 		ULONG Cpunumber = KeGetCurrentProcessorNumber();
 		*(ULONG*)(buffer + 2) = g_GDT[Cpunumber].gdtbase;
-        *(USHORT*)buffer = g_GDT[Cpunumber].gdtlimit;
+		*(USHORT*)buffer = g_GDT[Cpunumber].gdtlimit;
 		vmx_lgdt((ULONG*)buffer);
 		KeRevertToUserAffinityThread();
 		DbgPrint("Recover gdt at cpu[%d]\n", Cpunumber);
 	}
 }
 
-VOID SaveGDT() 
+VOID SaveGDT()
 {
 	for (int i = 0; i < KeNumberProcessors; i++)
 	{
